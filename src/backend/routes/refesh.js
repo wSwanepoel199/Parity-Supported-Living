a5971ec468e9472178a88f8d2d6dd2f107cd32f5
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-01-04 13:36:50 +1100

    removed backend from project

--- src/backend/routes/refesh.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const RefreshTokenController = require('../controllers/refreshToken.controller');
-
-const router = express.Router();
-
-router.get('/', RefreshTokenController.refreshToken);
-
-module.exports = router;
