3c4f7c106983c79613b45261b77d04038aa2f9f7
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-01-04 13:58:44 +1100

    updated backendApi to change url dependant on node env

--- src/shared/redux/api/backendApi.js
+++ src/shared/redux/api/backendApi.js
@@ -5,7 +5,7 @@ import { axiosBaseQuery } from "../../utils/api";
 export const backendApi = createApi({
   reducerPath: 'backendApi',
   baseQuery: axiosBaseQuery({
-    baseUrl: 'http://192.168.56.101:5000',
+    baseUrl: process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://192.168.1.117:5000",
   }),
   tagTypes: ['Post', 'User', 'Index'],
   endpoints: (builder) => ({
