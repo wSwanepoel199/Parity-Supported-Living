87546dc5b4d7e71509cc559d563a4518ed9475f5
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-08-04 14:35:35 +1000

    styled client view dialog

--- src/Pages/Client/ViewClient/ViewClient.js
+++ src/Pages/Client/ViewClient/ViewClient.js
@@ -1,4 +1,4 @@
-import { Backdrop, Box, Button, CircularProgress, Collapse, DialogActions, DialogContent, DialogTitle, FormControl, IconButton, InputLabel, Stack, Typography } from "@mui/material";
+import { Backdrop, Box, CircularProgress, Collapse, DialogContent, DialogTitle, FormControl, IconButton, InputLabel, Stack, Typography } from "@mui/material";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import CloseIcon from '@mui/icons-material/Close';
 import Grid from "@mui/material/Unstable_Grid2/";
@@ -57,27 +57,19 @@ const ViewClient = () => {
 
 
   return (
-    <Box>
+    <Box className={`dialog-background h-full`}>
       <DialogTitle className={`flex justify-between items-center`}>
-        <Typography variant="h6" component="p">
+        <Typography variant="h6" component="p" className={`txt-main`}>
           Viewing Client {data?.firstName} {data?.lastName}
         </Typography>
         <IconButton onClick={() => handleExit()}>
-          <CloseIcon />
+          <CloseIcon className={`interact-main`} />
         </IconButton>
       </DialogTitle>
       <DialogContent>
         <Grid container spacing={2} className="flex justify-center">
           <Grid xs={12}
-            className={` 
-            border-b-2
-             border-b-gray-400 
-             border-solid 
-             border-x-transparent 
-             border-t-transparent 
-             flex 
-             justify-between
-             group ${open.clientDetails && 'clientDetails'}`}
+            className={` border-b-2 border-b-gray-400 border-solid border-x-transparent border-t-transparent flex justify-between group ${open.clientDetails && 'clientDetails'}`}
             onClick={() => setOpen(prev => {
               return {
                 ...prev,
@@ -85,55 +77,48 @@ const ViewClient = () => {
               };
             })}
           >
-            <Typography>{formData.name}'s Details</Typography>
-            <ArrowBackIosNewIcon className={`transition group-[.clientDetails]:rotate-[-90deg] duration-100 ease-linear`} />
-
+            <Typography className={`txt-main`}>{formData.name}'s Details</Typography>
+            <ArrowBackIosNewIcon className={`txt-main transition group-[.clientDetails]:rotate-[-90deg] duration-100 ease-linear`} />
           </Grid>
           <Collapse
             in={open.clientDetails}
-            className={`
-              w-[90%]
-              `}
+            className={`w-full max-w-[90%]`}
           >
-            <Stack className={` border-[1px] border-slate-300 border-solid mt-[11px]`}>
-              <Grid container spacing={1} className="flex justify-center">
-                <Grid sm={6} xs={12} className="flex justify-center">
-                  <FormControl size="small" fullWidth margin="dense">
-                    <InputLabel shrink htmlFor="clientName" >Name</InputLabel>
-                    <Typography id="clientName" className={`p-3`}>{formData.name}</Typography>
-                  </FormControl>
-                </Grid>
-                <Grid sm={6} xs={12} className="flex justify-center">
-                  <FormControl size="small" fullWidth margin="dense">
-                    <InputLabel shrink htmlFor="clientPhoneNumber">Phone Number</InputLabel>
-                    <Typography id="clientPhoneNumber" className={`p-3`}>{formData.phoneNumber}</Typography>
-                  </FormControl>
-                </Grid>
-                <Grid sm={6} xs={12} className="flex justify-center">
-                  <FormControl size="small" fullWidth margin="dense">
-                    <InputLabel shrink htmlFor="clientEmail">Email</InputLabel>
-                    <Typography id="clientEmail" className={`p-3`}>{formData.email}</Typography>
-                  </FormControl>
-                </Grid>
-                <Grid sm={6} xs={12} className="flex justify-center ">
-                  <FormControl size="small" fullWidth margin="dense">
-                    <InputLabel shrink htmlFor="clientsAddress">Address</InputLabel>
-                    <Typography id="clientsAddress" className={`p-3`}>{formData.address}</Typography>
-                  </FormControl>
-                </Grid>
-                <Grid xs={12} className="flex justify-center">
-                  <FormControl size="small" fullWidth margin="dense">
-                    <InputLabel shrink htmlFor="notes">Details</InputLabel>
-                    <Typography id="notes" className={`p-3 pb-0`}>{formData.notes}</Typography>
-                  </FormControl>
-                </Grid>
+            <Grid container className={`border-[1px] border-slate-300 border-solid mt-[11px] p-3 m-2`}>
+              <Grid sm={6} xs={12} className="flex justify-center">
+                <FormControl size="small" fullWidth margin="dense">
+                  <InputLabel shrink htmlFor="clientName" className="txt-secondary">Name</InputLabel>
+                  <Typography id="clientName" className={`p-3 txt-secondary`}>{formData.name}</Typography>
+                </FormControl>
               </Grid>
-            </Stack>
+              <Grid sm={6} xs={12} className="flex justify-center">
+                <FormControl size="small" fullWidth margin="dense">
+                  <InputLabel shrink htmlFor="clientPhoneNumber" className="txt-secondary">Phone Number</InputLabel>
+                  <Typography id="clientPhoneNumber" className={`p-3 txt-secondary`}>{formData.phoneNumber}</Typography>
+                </FormControl>
+              </Grid>
+              <Grid sm={6} xs={12} className="flex justify-center">
+                <FormControl size="small" fullWidth margin="dense">
+                  <InputLabel shrink htmlFor="clientEmail" className="txt-secondary">Email</InputLabel>
+                  <Typography id="clientEmail" className={`p-3 txt-secondary`}>{formData.email}</Typography>
+                </FormControl>
+              </Grid>
+              <Grid sm={6} xs={12} className="flex justify-center ">
+                <FormControl size="small" fullWidth margin="dense">
+                  <InputLabel shrink htmlFor="clientsAddress" className="txt-secondary">Address</InputLabel>
+                  <Typography id="clientsAddress" className={`p-3 txt-secondary`}>{formData.address}</Typography>
+                </FormControl>
+              </Grid>
+              <Grid xs={12} className="flex justify-center">
+                <FormControl size="small" fullWidth margin="dense">
+                  <InputLabel shrink htmlFor="notes" className="txt-secondary">Details</InputLabel>
+                  <Typography id="notes" className={`p-3 pb-0 txt-secondary`}>{formData.notes}</Typography>
+                </FormControl>
+              </Grid>
+            </Grid>
           </Collapse>
           <Grid xs={12}
-            className={`border-b-2 border-b-gray-400 border-solid border-x-transparent border-t-transparent 
-            flex justify-between
-            group ${open.carersDetails && 'carersDetails'}`}
+            className={`border-b-2 border-b-gray-400 border-solid border-x-transparent border-t-transparent flex justify-between group ${open.carersDetails && 'carersDetails'}`}
             onClick={() => setOpen(prev => {
               return {
                 ...prev,
@@ -141,30 +126,30 @@ const ViewClient = () => {
               };
             })}
           >
-            <Typography>Carers Details</Typography>
+            <Typography className="txt-main">Carers Details</Typography>
             {formData.carers ?
-              <ArrowBackIosNewIcon className={`transition group-[.carersDetails]:rotate-[-90deg] duration-100 ease-linear`} />
+              <ArrowBackIosNewIcon className={`transition group-[.carersDetails]:rotate-[-90deg] duration-100 ease-linear txt-main`} />
               : null}
           </Grid>
           {formData.carers ?
             <Collapse
               in={open.carersDetails}
-              className={`w-[90%]`}
+              className={`w-full max-w-[90%]`}
             >
-              <Stack className={`divide-y divide-x-0 divide-slate-300 divide-solid border-[1px] border-slate-300 border-solid mt-[11px]`}>
+              <Stack className={`divide-y divide-x-0 divide-slate-300 divide-solid border-[1px] border-slate-300 border-solid mt-[11px] m-2`}>
                 {formData.carers.map((carer, index) => {
                   return (
-                    <Grid container spacing={0.5} key={index} className={`flex justify-between flex-wrap `}>
-                      <Grid sm={6} xs={12}>
-                        <FormControl size="small" margin="dense" className={`min-w-[200px]`}>
-                          <InputLabel shrink htmlFor="carerName">Carer</InputLabel>
-                          <Typography id="carerName" className={`p-3 pb-0`}>{`${carer.firstName} ${carer?.lastName}`}</Typography>
+                    <Grid container key={index} className={`flex justify-between flex-wrap py-3 sm:px-3`}>
+                      <Grid sm={6} xs={12} className="flex justify-center">
+                        <FormControl size="small" fullWidth margin="dense" >
+                          <InputLabel shrink htmlFor="carerName" className="txt-secondary">Carer</InputLabel>
+                          <Typography id="carerName" className={`p-3 pb-0 txt-secondary`}>{`${carer.firstName} ${carer?.lastName}`}</Typography>
                         </FormControl>
                       </Grid>
-                      <Grid sm={6} xs={12}>
-                        <FormControl size="small" margin="dense" className={`min-w-[200px]`}>
-                          <InputLabel shrink htmlFor="carerEmail">Carer's Email</InputLabel>
-                          <Typography id="carerEmail" className={`p-3 pb-0`}>{`${carer.email}`}</Typography>
+                      <Grid sm={6} xs={12} className="flex justify-center">
+                        <FormControl size="small" fullWidth margin="dense" >
+                          <InputLabel shrink htmlFor="carerEmail" className="txt-secondary">Carer's Email</InputLabel>
+                          <Typography id="carerEmail" className={`p-3 pb-0 txt-secondary`}>{`${carer.email}`}</Typography>
                         </FormControl>
                       </Grid>
                     </Grid>
@@ -233,11 +218,11 @@ const ViewClient = () => {
           </Collapse> */}
         </Grid>
       </DialogContent>
-      <DialogActions sx={{ justifyContent: 'end', alignContent: 'space-between', alignItems: 'center', px: '20px' }}>
+      {/* <DialogActions sx={{ justifyContent: 'end', alignContent: 'space-between', alignItems: 'center', px: '20px' }}>
         <Box>
           <Button onClick={() => handleExit()}>Close</Button>
         </Box>
-      </DialogActions>
+      </DialogActions> */}
     </Box>
   );
 };
