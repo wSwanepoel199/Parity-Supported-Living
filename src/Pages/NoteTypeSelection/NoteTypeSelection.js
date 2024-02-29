4429919fc2baadf4ccde84519027bb517d012e81
wSwanepoel <wjaco.swanepoel@gmail.com>
2024-02-29 11:31:29 +1100

    continued styling on note type selection page, size of bottons now scale with display, and text highlights when hovered over, also response to light or dark mode settings

--- src/Pages/NoteTypeSelection/NoteTypeSelection.js
+++ src/Pages/NoteTypeSelection/NoteTypeSelection.js
@@ -1,36 +1,64 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Container, Typography } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AddHomeOutlined, DomainOutlined } from '@mui/icons-material';
 
 const NoteTypeSelection = () => {
 
+  let communityNotes = useRef();
+  let silNotes = useRef();
   const navigate = useNavigate();
+  let [maxWidth, setMaxWidth] = useState(0);
 
+  useEffect(() => {
+    console.log(communityNotes.current.getBoundingClientRect().width - 4 - 32, silNotes.current.getBoundingClientRect().width - 4 - 32);
+    setMaxWidth(Math.max(communityNotes.current.getBoundingClientRect().width, silNotes.current.getBoundingClientRect().width) - 32);
+    console.log(maxWidth);
+  }, [maxWidth]);
 
   return (
-    <div className={`h-full flex justify-evenly items-center`}>
+    <Container
+      className={`h-full w-full max-w-2xl flex flex-col md:flex-row justify-center md:justify-around items-center`}
+    >
       <div
-        className={`text-white flex flex-col justify-center items-center rounded border-2 border-red-300 border-solid p-4`}
+        className={`text-psl-primary dark:text-psl-secondary-text hover:text-psl-active-link hover:dark:text-psl-active-link w-full max-w-xs p-4 m-2`}
+        id={`community-notes`}
+        ref={communityNotes}
+        style={{ 'height': `${maxWidth}px` }}
         onClick={() => {
           navigate('notes');
         }}
+      // onMouseEnter={() => {
+      //   communityNotes.current.className = `border-b-psl-active-link w-full max-w-xs p-4 m-2`;
+      // }}
+      // onMouseLeave={()=>{
+      //   communityNotes.current.className = `text-psl-primary dark:text-psl-secondary-text w-full max-w-xs p-4 m-2`;
+      // }}
       >
-        <DomainOutlined className={`w-full h-10 p-1`} />
-        <Typography >Community Notes</Typography>
-        {/* <Link to="community-notes">Community Notes</Link> */}
+        <div className={`h-full flex flex-col justify-center items-center rounded-md bg-gradient-to-t md:bg-gradient-to-b from-transparent from-5% to-psl-secondary-text dark:to-psl-primary-text shadow-md`}>
+          <DomainOutlined className={`w-full h-1/4 p-2`} />
+          <Typography >Community Notes</Typography>
+          {/* <Link to="community-notes">Community Notes</Link> */}
+        </div>
       </div>
       <div
-        className={`text-white flex flex-col justify-center items-center rounded border-2 border-red-300 border-solid p-4`}
+        className={`text-psl-primary dark:text-psl-secondary-text hover:text-psl-active-link hover:dark:text-psl-active-link w-full max-w-xs p-4 m-2`}
+        id={`sil-notes`}
+        ref={silNotes}
+        style={{ 'height': `${maxWidth}px` }}
         onClick={() => {
           navigate('sil-notes');
-        }}>
-        <AddHomeOutlined className={`w-full h-10 p-1`} />
-        <Typography >
-          SIL Notes
-        </Typography>
+        }}
+      >
+        <div className={`h-full flex flex-col justify-center items-center rounded-md bg-gradient-to-b from-transparent from-5% to-psl-secondary-text dark:to-psl-primary-text shadow-md`}>
+
+          <AddHomeOutlined className={`w-full h-1/4 p-2`} />
+          <Typography >
+            SIL Notes
+          </Typography>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
