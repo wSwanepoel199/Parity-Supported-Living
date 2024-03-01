96814d1615f85c7755b50ef0803ff43210173612
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2024-03-01 11:53:22 +1100

    removed unused code and unneeded console logs

--- src/Pages/NoteTypeSelection/NoteTypeSelection.js
+++ src/Pages/NoteTypeSelection/NoteTypeSelection.js
@@ -11,9 +11,9 @@ const NoteTypeSelection = () => {
   let [maxWidth, setMaxWidth] = useState(0);
 
   useEffect(() => {
-    console.log(communityNotes.current.getBoundingClientRect().width - 4 - 32, silNotes.current.getBoundingClientRect().width - 4 - 32);
+    // console.log(communityNotes.current.getBoundingClientRect().width - 4 - 32, silNotes.current.getBoundingClientRect().width - 4 - 32);
     setMaxWidth(Math.max(communityNotes.current.getBoundingClientRect().width, silNotes.current.getBoundingClientRect().width) - 32);
-    console.log(maxWidth);
+    // console.log(maxWidth);
   }, [maxWidth]);
 
   return (
@@ -28,17 +28,10 @@ const NoteTypeSelection = () => {
         onClick={() => {
           navigate('notes');
         }}
-      // onMouseEnter={() => {
-      //   communityNotes.current.className = `border-b-psl-active-link w-full max-w-xs p-4 m-2`;
-      // }}
-      // onMouseLeave={()=>{
-      //   communityNotes.current.className = `text-psl-primary dark:text-psl-secondary-text w-full max-w-xs p-4 m-2`;
-      // }}
       >
         <div className={`h-full flex flex-col justify-center items-center rounded-md bg-gradient-to-t md:bg-gradient-to-b from-transparent from-5% to-psl-secondary-text dark:to-psl-primary-text shadow-md`}>
           <DomainOutlined className={`w-full h-1/4 p-2`} />
           <Typography >Community Notes</Typography>
-          {/* <Link to="community-notes">Community Notes</Link> */}
         </div>
       </div>
       <div
