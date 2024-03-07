db25e5596a793bf53dc48cbe22fb3615cdfe0e51
wSwanepoel <wjaco.swanepoel@gmail.com>
2024-03-07 14:08:34 +1100

    removed console log in prompt.js

--- src/Components/Prompt/Prompt.jsx
+++ src/Components/Prompt/Prompt.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Box, Fab, Tooltip, useMediaQuery, useTheme } from '@mui/material';
-import DownloadIcon from '@mui/icons-material/Download';
-import UpdateIcon from '@mui/icons-material/Update';
-import deferredPromise from '../../Helpers/deferredPromise';
+import React from "react";
+import { Box, Fab, Tooltip, useMediaQuery, useTheme } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+import UpdateIcon from "@mui/icons-material/Update";
+import deferredPromise from "../../Helpers/deferredPromise";
 
 export const promptForUpdate = new deferredPromise();
 
@@ -10,15 +10,15 @@ const Prompt = ({ state, close }) => {
   const { type, message, data, status } = state;
 
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleAccept = async (type) => {
     switch (type) {
       case "install": {
         data.prompt();
-        const { outcome } = await data.userChoice;
-        console.log(`User Selectected ${outcome}`);
-        close(prev => {
+        // const { outcome } = await data.userChoice;
+        // console.log(`User Selectected ${outcome}`);
+        close((prev) => {
           return {
             ...prev,
             status: false,
@@ -28,10 +28,10 @@ const Prompt = ({ state, close }) => {
       }
       case "update": {
         promptForUpdate.resolve(true);
-        close(prev => {
+        close((prev) => {
           return {
             ...prev,
-            status: false
+            status: false,
           };
         });
         break;
@@ -42,13 +42,30 @@ const Prompt = ({ state, close }) => {
   };
 
   return (
-    <Box className='w-min flex box-border m-0 p-1'>
-      {status ?
-        <Tooltip title={message} placement="top" arrow classes={{ tooltip: 'text-base' }}>
-          <Fab size={fullScreen ? 'small' : 'medium'} aria-label={type} onClick={() => { handleAccept(type); }} className={`bg-psl-secondary-text text-psl-primary hover:text-psl-active-link dark:bg-psl-secondary dark:text-psl-active-text hover:dark:text-psl-active-link`}>
-            {type === "install" ? <DownloadIcon /> : type === 'update' ? <UpdateIcon /> : null}
+    <Box className="w-min flex box-border m-0 p-1">
+      {status ? (
+        <Tooltip
+          title={message}
+          placement="top"
+          arrow
+          classes={{ tooltip: "text-base" }}
+        >
+          <Fab
+            size={fullScreen ? "small" : "medium"}
+            aria-label={type}
+            onClick={() => {
+              handleAccept(type);
+            }}
+            className={`bg-psl-secondary-text text-psl-primary hover:text-psl-active-link dark:bg-psl-secondary dark:text-psl-active-text hover:dark:text-psl-active-link`}
+          >
+            {type === "install" ? (
+              <DownloadIcon />
+            ) : type === "update" ? (
+              <UpdateIcon />
+            ) : null}
           </Fab>
-        </Tooltip> : null}
+        </Tooltip>
+      ) : null}
     </Box>
   );
 };
